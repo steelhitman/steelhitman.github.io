8c6d39b994a3bb366f09737202e2d14fc070c48f
Prashant Arya <steelhitman@users.noreply.github.com>
2019-10-09 19:23:18 +0530

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,9 +1,9 @@
 const video=document.getElementById("video")
 function startVideo(){
-	navigator.getUserMedia({
-		video: {0};
-		stream => video.srcObject=stream
+	navigator.getUserMedia(
+		{video: {}},
+		stream => video.srcObject=stream,
 		err=> console.error(err)
-	})
+	)
 }	
 startVideo()
